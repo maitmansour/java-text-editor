d39ec476da8260280b80f7b5f493ac1f8e022310
Maxime Pinot <maxime@maximepinot.com>
2017-03-15 16:42:28 +0100

    add final part 1

--- src/partie1/SecondWindow.java
+++ src/partie1/SecondWindow.java
@@ -9,25 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ButtonGroup;
-import javax.swing.DefaultListCellRenderer;
-import javax.swing.InputMap;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JTextArea;
-import javax.swing.JToolBar;
-import javax.swing.KeyStroke;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 
 public class SecondWindow extends JFrame
 {
@@ -63,23 +45,21 @@ public class SecondWindow extends JFrame
 		couleurGroup.add(blue);
 		couleurGroup.add(rouge);
 		couleurGroup.add(noir);
-
 		couleur.add(blue);
 		couleur.add(rouge);
 		couleur.add(noir);
 
+		// Raccourcis
 		rouge.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,InputEvent.CTRL_MASK));
 		blue.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B,InputEvent.CTRL_MASK));
 		noir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,InputEvent.CTRL_MASK));
 
-
-		
-
+		// La zone de texte
 		textArea = new JTextArea("");
 		textArea.requestFocus();
 		add(textArea);
-		
 
+		// Le menu style
 		JMenu style = new JMenu("Style");
 		menuBar.add(style);
 		setJMenuBar(menuBar);
@@ -93,12 +73,10 @@ public class SecondWindow extends JFrame
 		styleGras.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G,InputEvent.CTRL_MASK));
 		styleItalique.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_I,InputEvent.CTRL_MASK));
 		
-		
-		//setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,InputEvent.CTRL_MASK+InputEvent.SHIFT_MASK));
-		
-		
+
 		// Toolbar
 		initToolBar();
+
 
 		pack();
 		setLocationRelativeTo(null);
@@ -151,12 +129,10 @@ public class SecondWindow extends JFrame
 		InputMap im = reset.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         im.put(KeyStroke.getKeyStroke(KeyEvent.VK_R,InputEvent.CTRL_MASK+InputEvent.SHIFT_MASK), "sar");
         reset.getActionMap().put("sar", new ResetAction());
-		
-		//reset.setMnemonic(KeyEvent.VK_F5);
+
 		add(toolBar, BorderLayout.NORTH);
 	}
 
-	
 	
 	class ResetAction extends AbstractAction {
 
@@ -190,33 +166,28 @@ public class SecondWindow extends JFrame
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
-			int boldState= Font.PLAIN;
+			int boldState = Font.PLAIN;
 			int italicState = Font.PLAIN;
 
 			if (styleGras.isSelected() || tbBold.isSelected())
 			{
 				boldState = Font.BOLD;
-				// styleGras.setSelected(true);
-				// tbBold.setSelected(true);
 			}
 
 			if (styleItalique.isSelected() || tbItalic.isSelected())
 			{
 				italicState = Font.ITALIC;
-				// styleItalique.setSelected(true);
-				// tbItalic.setSelected(true);
 			}
 
-			textArea.setFont(textArea.getFont().deriveFont(boldState+italicState));
-
-
+			textArea.setFont(textArea.getFont().deriveFont(boldState + italicState));
 		}
 		
 	}
 	
 	class ColorMenuItem extends AbstractAction{
 		private Color color;
-		public ColorMenuItem(String nom , Color color ) {
+
+		ColorMenuItem(String nom , Color color ) {
 			putValue(Action.NAME, nom);
 			this.color = color;
 		}
